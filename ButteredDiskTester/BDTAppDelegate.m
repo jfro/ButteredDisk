3bba561cee0020e1eccc0fcc82ef590348b4f499
Jeremy Knope <jerome@buttered-cat.com>
2013-04-14 12:56:55 -0400

    Initial Commit

---
+++ ButteredDiskTester/BDTAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  BDTAppDelegate.m
+//  ButteredDiskTester
+//
+//  Created by Jeremy Knope on 4/14/13.
+//  Copyright (c) 2013 Buttered Cat Software. All rights reserved.
+//
+
+#import "BDTAppDelegate.h"
+
+@implementation BDTAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
